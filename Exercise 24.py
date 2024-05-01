294e0fe1e242f40aa378d7f1af86cb92d9cdedeb
Siesta0922 <148171376+Siesta0922@users.noreply.github.com>
2024-05-01 08:57:53 +0800

    Update Exercise 24.py

--- Exercise 24.py
+++ Exercise 24.py
@@ -1,1 +1,12 @@
+def is_leap(year)
+  if year % 4 == 0:
+    return "Leap Year"
+  else:
+    return "Not A Leap Year"
+def days_in_month():
+  month_days = [31,28,31,30,31,30,31,31,30,31,30,31]
 
+year = int(input("Year"))
+month = int(input("Month"))
+days = days_in_month(year,month)
+print(days)
